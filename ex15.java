48fe34135e9f13791ba92432e93364b3b1e8954c
Mikecruze <162t0307@itsm.edu.mx>
2017-05-25 23:23:14 -0500

    Create ex15.java

---
+++ ex15.java
@@ -0,0 +1,26 @@
+package exa;
+
+public class ex15{
+
+	class ServerTimeOutException extends Exception {
+
+		private static final long serialVersionUID = 1L;}
+
+	private static final Exception ServerTimeOutException = null;
+	
+	public void conectame( String nombreServidor ) throws Exception {
+	
+		int exito;
+	
+		int puerto = 80;
+	exito = open( nombreServidor,puerto );
+    
+	if( exito == -1 )
+	throw ServerTimeOutException;
+	
+	}
+
+	private int open(String nombreServidor, int puerto) {
+		return 0;
+	}
+}
