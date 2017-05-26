4041a1145bc74bf3bde553fe3a8088c43232fee9
Mikecruze <162t0307@itsm.edu.mx>
2017-05-25 23:16:16 -0500

    Create ex11.java

---
+++ ex11.java
@@ -0,0 +1,52 @@
+package exa;
+
+@SuppressWarnings("unused")
+public class ex11 {
+
+	class Limites extends Exception {
+
+	
+		private static final long serialVersionUID = 1L;}
+	@SuppressWarnings("serial")
+	class demasiadoCalor extends Limites {}
+	class demasiadoFrio extends Limites {
+
+		
+		private static final long serialVersionUID = 1L;}
+	class demasiadoRapido extends Limites {
+
+
+		private static final long serialVersionUID = 1L;}
+	class demasiadoCansado extends Limites {
+
+		private static final long serialVersionUID = 1L; {
+				
+			    
+	try {
+	        int temp = 0;
+			if( temp > 40 )
+	            throw( new demasiadoCalor() );			
+			int dormir = 0;
+	        if( dormir < 8 )
+	        	
+	throw( new <Exception> Exception());
+	    } catch( Limites lim ) {
+	        if( lim instanceof demasiadoCalor ) {
+	            System.out.println( "Capturada excesivo calor!" );
+	            
+	            }
+	        if( lim instanceof demasiadoCansado ) {
+	            System.out.println( "Capturada excesivo cansancio!" );
+	            
+	            }
+	    } catch (Exception e) {
+			e.printStackTrace();
+		}  finally {
+	        System.out.println( "En la clausula finally" );
+	     
+
+	}
+		}
+	}
+}
+	  
